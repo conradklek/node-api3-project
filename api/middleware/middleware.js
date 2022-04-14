a14ba3a2fd26edefefcb702b45260cd435c78299
GooieJS <conradklek@gmail.com>
2022-04-13 23:45:51 -0500

    push

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,44 @@
+const User = require('../users/users-model');
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  const timestamp = new Date().toLocaleString();
+  const method = req.method;
+  const url = req.originalUrl;
+  console.log(`${timestamp} ${method} to ${url}`);
+  next()
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+async function validateUserId(req, res, next) {
+  try {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      req.user = user;
+      next();
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to validate user' });
+  }
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+ const { name } = req.body;
+  if (!name || !name.trim()) {
+    res.status(400).json({ message: 'Missing required name field' });
+  } else {
+    req.name = name.trim();
+    next();
+  }
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  
 }
 
-// do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
+}
